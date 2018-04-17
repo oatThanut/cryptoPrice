cb26bb3c6caed4e822f7d7474c53d11e2742bb13
oatThanut <thanut.sjknk@gmail.com>
2018-04-17 09:21:41 +0700

    Initial Commit

---
+++ CryptoPrice/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CryptoPrice
+//
+//  Created by oatThanut on 17/4/2561 BE.
+//  Copyright © 2561 oatThanut. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
