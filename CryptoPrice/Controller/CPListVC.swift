b6812e8fffbe516aa29849cb05d43a42d637f310
oatThanut <thanut.sjknk@gmail.com>
2018-05-09 09:15:16 +0700

    Connect list view with controller

--- CryptoPrice/Controller/CPListVC.swift
+++ CryptoPrice/Controller/CPListVC.swift
@@ -29,7 +29,7 @@ class CPListVC: UITableViewController {
 
     override func numberOfSections(in tableView: UITableView) -> Int {
         // #warning Incomplete implementation, return the number of sections
-        return 0
+        return 1
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
@@ -37,15 +37,15 @@ class CPListVC: UITableViewController {
         return 0
     }
 
-    /*
+    
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: "ListCell", for: indexPath)
 
         // Configure the cell...
 
         return cell
     }
-    */
+    
 
     /*
     // Override to support conditional editing of the table view.
